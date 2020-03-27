6b5cb46054a153c6347df2cd268b818ca5bdafd3
florenciagonzalezteti96 <florenciagt.uni@gmail.com>
2020-03-27 20:32:35 -0300

    Agrego ejercicios 03 y 04

---
+++ Guia de Ejercicios/Ejer_03-04/Ejer_03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+/**
+Mostrar por pantalla todos los números primos que haya hasta el número que ingrese el usuario
+por consola.
+*/
+namespace Ejer_03
+{
+    class Ejer_03
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+            int i;
+            int j;
+            int flag;
+
+            Console.WriteLine("Ingrese un numero:");
+            numeroIngresado = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Los numeros primos anteriores a {0} son:", numeroIngresado);
+
+            for (i=2 ; i<=numeroIngresado ; i++)
+            {
+                flag = 0;
+
+                    for(j=2 ; j<i-1 && flag == 0 ; j++)
+                    {
+                        if (i % j == 0)
+                        { 
+                            flag = 1;
+                        }
+                    }
+                    if (flag == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Guia de Ejercicios/Ejer_03-04/Ejer_04/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+/**
+ Un número perfecto es un entero positivo, que es igual a la suma de todos los enteros positivos
+(excluido el mismo) que son divisores del número.
+El primer número perfecto es 6, ya que los divisores de 6 son 1, 2 y 3; y 1 + 2 + 3 = 6.
+Escribir una aplicación que encuentre los 4 primeros números perfectos.
+*/
+
+namespace Ejer_04
+{
+    class Ejer_04
+    {
+        static void Main(string[] args)
+        {
+            int contadorNumerosPerfectos = 0;
+            int acumuladorDeDivisores = 0;
+            int i = 1; //Numero actual
+            int j; //Representa todos los numeros
+
+            Console.WriteLine("Los primeros 4 numeros perfectos son: ");
+
+            while (contadorNumerosPerfectos != 4)
+            {
+                acumuladorDeDivisores = 0; //Este hay que resetearlo para cada numero evaluado
+                for (j = 1; j < i; j++) //Recorro los numeros a partir del 1 hasta mi numero actual
+                {
+                    if (i % j == 0) //Aca te fijas si es divisor, entonces se suman
+                    {
+                        acumuladorDeDivisores += j;
+                    }
+                }
+                if (acumuladorDeDivisores == i) //Aca mostrar al numero perfecto
+                {
+                    contadorNumerosPerfectos++;
+                    Console.Write(i + " ");
+                }
+                i++; //Aca cambio al numero siguiente
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
