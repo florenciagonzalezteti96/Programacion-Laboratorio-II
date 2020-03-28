063b743e6b576b0ccadfbe953814632c94443942
florenciagonzalezteti96 <florenciagt.uni@gmail.com>
2020-03-27 23:54:24 -0300

    Agrego ejercicios 05 y 06

---
+++ Guia de Ejercicios/Ejer_05-06/Ejer_05/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+/** Un centro numérico es un número que separa una lista de números enteros (comenzando en 1) en 
+dos grupos de números, cuyas sumas son iguales.
+El primer centro numérico es el 6, el cual separa la lista (1 a 8) en los grupos: (1; 2; 3; 4; 5) y (7; 8) 
+cuyas sumas son ambas iguales a 15. El segundo centro numérico es el 35, el cual separa la lista (1 a 
+49) en los grupos: (1 a 34) y (36 a 49) cuyas sumas son ambas iguales a 595.
+Se pide elaborar una aplicación que calcule los centros numéricos entre 1 y el número que el 
+usuario ingrese por consola.
+*/
+
+namespace Ejer_05
+{
+    class Ejer_05
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;//guardo el numero ingresado
+            int i;
+            int j;
+            int m;
+            int sumaDeAnteriores;//acumulador de todos los numeros anteriores al num a analizar
+            int sumaDePosteriores;//acumulador de todos los numeros anteriores al num a analizar
+            int ningunCentro = 0;
+
+            Console.WriteLine("Ingrese un numero para calcular todos los centros numericos previos al mismo: ");
+            numeroIngresado = Convert.ToInt32(Console.ReadLine());
+
+            for(i=2; i<numeroIngresado; i++)
+            {
+                sumaDeAnteriores = 0;
+                sumaDePosteriores = 0;
+
+                for(j=1;j<i;j++)
+                {
+                    sumaDeAnteriores = j + sumaDeAnteriores;
+                }
+
+                for(m=i+1; sumaDePosteriores < sumaDeAnteriores; m++)
+                {
+                    sumaDePosteriores = m + sumaDePosteriores;
+
+                    if(sumaDePosteriores == sumaDeAnteriores)
+                    {
+                        Console.WriteLine(i + " ");
+                        ningunCentro = 1;
+                        break;
+                    }
+                }
+            }
+            if(ningunCentro == 0)
+            {
+                Console.WriteLine("No se encontro ningun centro numerico.");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Guia de Ejercicios/Ejer_05-06/Ejer_06/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+/**
+Escribir un programa que determine si un año es bisiesto.
+Un año es bisiesto si es múltiplo de 4. Los años múltiplos de 100 no son bisiestos, salvo si ellos
+también son múltiplos de 400. Por ejemplo: el año 2000 es bisiesto pero 1900 no.
+Pedirle al usuario un año de inicio y otro de fin y mostrar todos los años bisiestos en ese rango.
+*/
+namespace Ejer_06
+{
+    class Ejer_06
+    {
+        static void Main(string[] args)
+        {
+            int anioInicial;
+            int anioFinal;
+            int i;
+            int noHayBisiestos = 0;
+
+            Console.WriteLine("Ingrese un anio inicial: ");
+            anioInicial = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese un anio final: ");
+            anioFinal = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Los anios bisiestos entre {0} y {1} son:", anioInicial, anioFinal);
+
+            for(i=anioInicial;i<anioFinal;i++)
+            {
+                if(i%4 == 0)// si o si tiene que ser multiplo de 4
+                {
+                    if(i%100 == 0)//si es mult de 4 y de 100
+                    {
+                        if(i%400 == 0)//tiene que ser tambien mult de 400
+                        {
+                            Console.Write(i + " ");
+                            noHayBisiestos = 1;
+                        }
+                    }
+                    else //si no es multiplo de 100 pero si de 4
+                    {
+                        Console.Write(i + " ");
+                        noHayBisiestos = 1;
+                    }  
+                }
+            }
+
+            if(noHayBisiestos == 0)
+            {
+                Console.WriteLine("\nNo hay anios bisiestos entre {0} y {1}", anioInicial, anioFinal);
+            }
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
