5e1a39ffdaf2bd443eda1c93a743efa899d4076f
florenciagonzalezteti96 <florenciagt.uni@gmail.com>
2020-04-11 17:21:13 -0300

    Agrego ejercicio integrador de clase 05

---
+++ Guia de Ejercicios/Ejercicio Integrador Clase 05/Ejercicio_Integrador_Clase05/Estante.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Producto auxProducto in e.productos)
+            {
+                sb.AppendLine(Producto.MostrarProducto(auxProducto));
+            }
+
+            return sb.ToString();
+        }
+        public static bool operator ==(Estante e, Producto p)
+        {
+            /*bool existeElProducto = false;
+            foreach (Producto auxProducto in e.productos)
+            {
+                if(!(auxProducto is null))//si el producto existe
+                {
+                    if(auxProducto == p)
+                    {
+                        existeElProducto = true;
+                    }
+                }
+            }
+            return existeElProducto;*/
+
+            return e.productos.Contains(p);
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e.productos.Contains(p));
+        }
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool sePudoAgregar = false;
+            if (e != p)//si el producto no existe en el estante ya
+            {
+                for (int i = 0; i < e.productos.Length; i++)//recorro el estante
+                {
+                    if (e.productos[i] is null)//si hay espacio
+                    {
+                        e.productos[i] = p;
+                        sePudoAgregar = true;
+                        break;
+                    }
+                }
+            }
+
+            return sePudoAgregar;
+        }
+        public static Estante operator -(Estante e, Producto p)
+        {
+            bool sePudoQuitar = false;
+            for (int i = 0; i < e.productos.Length; i++)//recorro el estante
+            {
+                if (e.productos[i] == p)//si existe en el estante
+                {
+                    e.productos[i] = null;
+                    sePudoQuitar = true;
+                    break;
+                }
+            }
+            return e;
+        }
+    }
+}
---
+++ Guia de Ejercicios/Ejercicio Integrador Clase 05/Ejercicio_Integrador_Clase05/Producto.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigo;
+            this.precio = precio;
+        }
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Marca del producto: " + p.marca);
+            sb.AppendLine("Codigo de barra del producto: " + p.codigoDeBarra);
+            sb.AppendLine("Precio del producto: " + p.precio);
+
+            return sb.ToString();
+        }
+        public static explicit operator string(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(p.codigoDeBarra);
+
+            return sb.ToString();
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool retorno = false;
+            if(p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+        public static bool operator ==(Producto p1, string marca)
+        {
+            bool retorno = false;
+            if (p1.marca == marca)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+        public static bool operator !=(Producto p1, string marca)
+        {
+            return !(p1 == marca);
+        }
+
+
+    }
+}
